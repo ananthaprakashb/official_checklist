eeb641ba51946e674f6d5dd03feb0ca2b9f34d8c
Ananthaprakash <ananthaprakashb@gmail.com>
2026-08-11 16:24:24 -0700

    fix: cloudflare configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [cloudflare()],
   base: command === "build" ? (process.env.VITE_BASE_PATH || "/official_checklist/") : "/"
 }));
